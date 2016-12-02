8bfffe1c5587a3ed107b42246e633d14e0126925
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-02 11:38:13 -0500

    seeded users

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,9 @@
 class MainController < ApplicationController
-
-	def home
-		
-		if user_signed_in?
-			
+	def home		
+		if user_signed_in?			
 			render :signed_in_home
 		else
 			render :home
 		end
-
 	end
-
-
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email:"johnny@boy.com", password: "blahblah", password_confirmation: "blahblah", first_name: "Johnny", last_name: "Boy", bio: "Chef in training.", industry: "Restaurant", location: "New York, NY")
+User.create!(email:"juan@juan.com", password: "whatever", password_confirmation: "whatever", first_name: "Juan", last_name: "Perez", bio: "Bartender", industry: "Bar", location: "Miami Beach, FL")
+User.create!(email:"james@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "James", last_name: "Franco", bio: "", industry: "Bar", location: "Los Angeles, CA")
+User.create!(email:"peyton@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Peyton", last_name: "Manning", bio: "Pastry Chef", industry: "Hotel", location: "Denver, CO")
