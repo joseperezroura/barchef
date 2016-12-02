9f62528dae91cd048117b48435d177c8e62131a7
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-02 12:37:21 -0500

    embedded ruby into signed in profile page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,12 @@
 class MainController < ApplicationController
 	def home		
 		if user_signed_in?			
+			@user = User.first(6)
 			render :signed_in_home
 		else
 			render :home
 		end
 	end
+
+
 end
