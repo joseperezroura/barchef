30433452390d7e51b9de4963a4ebedcd519758c5
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-02 20:35:02 -0500

    seeded new users and users with recipes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 def show
 	@user = User.find(params[:id])
-
+	@recipes = @user.recipes
 	render :show
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email:"johnny@boy.com", password: "blahblah", password_confirmation: "blahblah", first_name: "Johnny", last_name: "Boy", bio: "Chef in training.", industry: "Restaurant", location: "New York, NY")
-User.create!(email:"juan@juan.com", password: "whatever", password_confirmation: "whatever", first_name: "Juan", last_name: "Perez", bio: "Bartender", industry: "Bar", location: "Miami Beach, FL")
-User.create!(email:"james@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "James", last_name: "Franco", bio: "", industry: "Bar", location: "Los Angeles, CA")
-User.create!(email:"peyton@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Peyton", last_name: "Manning", bio: "Pastry Chef", industry: "Hotel", location: "Denver, CO")
+johnny = User.create!(email:"johnny@boy.com", password: "blahblah", password_confirmation: "blahblah", first_name: "Johnny", last_name: "Boy", bio: "Chef in training.", industry: "Restaurant", location: "New York, NY")
+juan = User.create!(email:"juan@juan.com", password: "whatever", password_confirmation: "whatever", first_name: "Juan", last_name: "Perez", bio: "Bartender", industry: "Bar", location: "Miami Beach, FL")
+james = User.create!(email:"james@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "James", last_name: "Franco", bio: "", industry: "Bar", location: "Los Angeles, CA")
+peyton = User.create!(email:"peyton@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Peyton", last_name: "Manning", bio: "Pastry Chef", industry: "Hotel", location: "Denver, CO")
+molly = User.create!(email:"molly@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Molly", last_name: "Smith", bio: "", industry: "Bar", location: "SanDiego, CA")
+eli = User.create!(email:"eli@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Eli", last_name: "Manning", bio: "Bartender", industry: "Hotel", location: "New York,NY")
+
+johnny.recipes.create!(name:"blah", description:"blah served with ice", instructions:"lorem ipsum you are a bad mofo and I am the best.")
+johnny.recipes.create!(name:"foobarmagic", description:"ingredient1:blaaaaaaaaaaaah", instructions:"blah bald nooooooo hijo de la chingada")
