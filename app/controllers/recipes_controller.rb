97d2ae8aca6856800273d8a2b736d0dd8cd14602
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-03 19:04:16 -0500

    more changes to create method of recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,40 @@
 class RecipesController < ApplicationController
 
 def new
+	@recipe = Recipe.new
 	render :new
+	# @recipe.save
+end
+
+def create
+
+
+
+	@recipe = Recipe.new(params[:id])
+
+	# @user = @recipe.recipe_entries.new(user_params)
+
+	# @user.save
+			
+	
+	redirect_to(user_session_path)
+
+	# @recipe = Recipe.create(params[:recipe_id])		
+		
+	# 	@recipe.save
+	# 		render :new
+	# 	end
 end
 
 
+
+
+
+	# private
+	# def recipe_params
+	# 	params.require(:recipe).permit(:name, :description, :instructions)
+
+	# 	# params[:time_entry][:hours]
+	# end
 
 end
