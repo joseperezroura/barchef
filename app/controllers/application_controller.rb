e0dab4f06a5e0d7bf299d034a95fa190ab123fb5
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-07 10:22:54 -0500

    fixed formatting issue in profile show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   	def setup_extra_devise_fields
   	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :bio, :industry, :company, :location, :avatar])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :bio, :industry, :company, :location, :avatar])
   	# devise_parameter_sanitizer.permit(:profile?????, keys: [:first_name, :last_name])
   end
 end
