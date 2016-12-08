d1ffb02ea71b0cf8e76b2c89e6536978dda4fb1c
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-08 18:32:37 -0500

    formatting errors corrected and missing photo icons inserted

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name, :bio, :industry, :location
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/chef.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_many :recipes
