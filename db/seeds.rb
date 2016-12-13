65e69c17b3ae7a8040081b311014e1c721cf25c2
Jose Perez-Roura <jose.perezroura@gmail.com>
2016-12-13 09:14:13 -0500

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-johnny = User.create!(email:"johnny@boy.com", password: "blahblah", password_confirmation: "blahblah", first_name: "Johnny", last_name: "Boy", bio: "Chef in training.", industry: "Restaurant", location: "New York, NY")
+johnny = User.create!(email:"johnny@boy.com", password: "blahblah", password_confirmation: "blahblah", first_name: "Johnny", last_name: "Boy", bio: "Bartender", industry: "27 Restaurant", location: "Miami Beach, FL")
 juan = User.create!(email:"juan@juan.com", password: "whatever", password_confirmation: "whatever", first_name: "Juan", last_name: "Perez", bio: "Bartender", industry: "Bar", location: "Miami Beach, FL")
 james = User.create!(email:"james@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "James", last_name: "Franco", bio: "Bartender in training at Broken Shaker. Living like its my last day brah...", industry: "Bar", location: "Los Angeles, CA")
 peyton = User.create!(email:"peyton@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Peyton", last_name: "Manning", bio: "Pastry Chef", industry: "Hotel", location: "Denver, CO")
 molly = User.create!(email:"molly@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Molly", last_name: "Smith", bio: "Cocktail Server at your mom's house.", industry: "Bar", location: "SanDiego, CA")
 eli = User.create!(email:"eli@gmail.com", password: "whatever", password_confirmation: "whatever", first_name: "Eli", last_name: "Manning", bio: "Bartender for the New York Football Giants.", industry: "Hotel", location: "New York,NY")
 
-johnny.recipes.create!(name:"blah", description:"blah served with ice", instructions:"lorem ipsum you are a bad mofo and I am the best.")
-johnny.recipes.create!(name:"foobarmagic", description:"ingredient1:blaaaaaaaaaaaah", instructions:"blah bald nooooooo hijo de la chingada")
+johnny.recipes.create!(name:"Old Fashioned", description:"2oz Bourbon Whiskey, barspoon of Simple Syrup and Bitters.", instructions:"Put all the ingredients in a mixing glass, add ice and stir for 40 seconds.  Serve over ice with a orange peel.")
+johnny.recipes.create!(name:"Rucker Park", description:"1oz Rye Whiskey, 1oz Cherry Heering, 1oz Lemon Juice and .25oz of honey.", instructions:"Shake all the ingredients with ice.  Strain into a rocks glass filled with ice.  Garnish with a cherry.")
